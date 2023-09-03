c8df652ec59347f48c313de1edcb4b85f148c790
jakub.ivanic <ivanic.jakub1@gmail.com>
2023-09-03 19:23:26 +0200

    Scene view issue fixed + Render issue fixed
    - Use MotionBlurKeywordHandler.cs in your scene to use camera blacklist render restriction

--- Source/ObjectBlurNode.hlsl
+++ Source/ObjectBlurNode.hlsl
@@ -6,20 +6,25 @@
 
 void MyFunction_float(UnityTexture2D colorTex,float maxSamples, float2 UV,float2 velocity, out float4 result)
 {
+    #if defined(_ISGAMEVIEWANDPLAYING)
     // float speed = length(velocity * (1.0 / float2(1080,1920)));
     // float nSamples = clamp(int(speed), 1, maxSamples);
     float nSamples = maxSamples;
     
     // result = SAMPLE_TEXTURE2D(_BlitTexture , sampler_LinearClamp ,UV);
-    result = SAMPLE_TEXTURE2D(colorTex.tex , colorTex.samplerstate ,UV);
+    result = SAMPLE_TEXTURE2D(colorTex.tex , sampler_LinearClamp ,UV);
     
     [unroll(100)] for (int i = 1; i < nSamples; ++i) {
         float2 offset = velocity * (float(i) / float(nSamples - 1) - 0.5);
         // result += SAMPLE_TEXTURE2D(_BlitTexture ,sampler_LinearClamp,UV+offset);
-        result += SAMPLE_TEXTURE2D(colorTex.tex , colorTex.samplerstate,UV+offset);
+        result += SAMPLE_TEXTURE2D(colorTex.tex , sampler_LinearClamp,UV+offset);
         
     }
     result /= float(nSamples);
     // result = float4(result.rgb,1);
+    #else
+    result = SAMPLE_TEXTURE2D(colorTex.tex , colorTex.samplerstate ,UV);
+    #endif
+    
 }
 #endif
